b24fab0b568e338018deb4abb46d51b8ac51576a
0xDev05 <divpatel1274@gmail.com>
2025-08-07 16:23:36 +0530

    admin side implemenation completed

--- programs/credentia/src/instructions/initialize.rs
+++ programs/credentia/src/instructions/initialize.rs
@@ -1,9 +1,53 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint, Token};
 
+use crate::Platform;
+
+
+//initializing platform(admin)
 #[derive(Accounts)]
-pub struct Initialize {}
-
-pub fn handler(ctx: Context<Initialize>) -> Result<()> {
-    msg!("Greetings from: {:?}", ctx.program_id);
-    Ok(())
+pub struct Initialize<'info> {
+    #[account(mut)]   
+    pub admin: Signer<'info>,
+    #[account(
+        init,
+        payer = admin,
+        space = 8 + Platform::INIT_SPACE,
+        seeds = [b"platform"],
+        bump,
+    )]
+    pub platform: Account<'info, Platform>,
+    #[account(
+        seeds = [b"treasury_vault", platform.key().as_ref()],
+        bump,
+    )]
+    pub treasury_vault: SystemAccount<'info>,
+    #[account(
+        init, 
+        payer = admin,
+        seeds = [b"reward_mint", platform.key().as_ref()],
+        bump,
+        mint::decimals = 6,
+        mint::authority = platform,
+    )]
+    pub reward_mint: Account<'info, Mint>,
+    pub token_program: Program<'info , Token>,
+    pub system_program: Program<'info, System>,
 }
+
+impl<'info> Initialize<'info>{
+    pub fn init(&mut self , fee_bps: u16 , bumps:&InitializeBumps) -> Result<()> {
+        
+        self.platform.set_inner(Platform{
+            authority: self.admin.key(),
+            fee_bps,
+            reward_bump: bumps.reward_mint,
+            treasury_bump: bumps.treasury_vault,
+            bump:bumps.platform,
+        });
+
+        Ok(())
+    }
+}
+
+
---
+++ programs/credentia/src/state/platform.rs
@@ -0,0 +1,11 @@
+use anchor_lang::prelude::*;
+
+#[account]
+#[derive(InitSpace)]
+pub struct Platform{
+    pub authority: Pubkey,
+    pub fee_bps: u16,
+    pub reward_bump: u8,
+    pub treasury_bump: u8,
+    pub bump: u8,
+}

585c238b523d34cb11c51e0c41226eeb7099972d
0xDev05 <divpatel1274@gmail.com>
2025-08-09 01:01:47 +0530

    Default Loan Functionality Completed

---
+++ programs/credentia/src/instructions/default_loan.rs
@@ -0,0 +1,124 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{close_account, transfer_checked, CloseAccount, TransferChecked},
+    token_interface::{Mint, TokenAccount, TokenInterface},
+};
+
+use crate::{error::ErrorCode, events::NFTClaimed, Loan, LoanStatus, Platform};
+
+#[derive(Accounts)]
+pub struct DefaultLoan<'info> {
+    #[account(mut)]
+    pub lender: Signer<'info>,
+    #[account(mut)]
+    pub borrower: SystemAccount<'info>,
+    pub borrower_nft_mint: InterfaceAccount<'info, Mint>,
+    #[account(
+        init_if_needed,
+        payer = lender,
+        associated_token::mint = borrower_nft_mint,
+        associated_token::authority = lender,
+    )]
+    pub lender_nft_ata: InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        seeds = [b"platform"],
+        bump = platform.bump,
+    )]
+    pub platform: Account<'info, Platform>,
+    #[account(
+        mut,
+        has_one = borrower,
+        close = borrower,
+        seeds = [b"loan" , borrower_nft_mint.key().as_ref() , platform.key().as_ref()],
+        bump = loan_account.bump,
+    )]
+    pub loan_account: Account<'info, Loan>,
+    #[account(
+        mut,
+        associated_token::mint = borrower_nft_mint,
+        associated_token::authority = loan_account,
+    )]
+    pub nft_vault: InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        seeds = [b"treasury_vault", platform.key().as_ref()],
+        bump,
+    )]
+    pub treasury_vault: SystemAccount<'info>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+impl<'info> DefaultLoan<'info> {
+    pub fn claim_nft(&mut self) -> Result<()> {
+        let start_time = self
+            .loan_account
+            .start_time
+            .ok_or(ErrorCode::LoanNotStarted)?;
+        require!(
+            Clock::get()?.unix_timestamp - start_time >= self.loan_account.duration as i64,
+            ErrorCode::WaitForLoanToComplete
+        );
+
+        //NOTE: if you have to change option to dafault you have to change this
+        require!(
+            self.loan_account.lender == Some(self.lender.key()),
+            ErrorCode::LenderNotMatched
+        );
+        require!(
+            self.loan_account.status != LoanStatus::Repaid,
+            ErrorCode::LoanAlreadyRepaid
+        );
+        require!(
+            self.loan_account.status != LoanStatus::Defaulted,
+            ErrorCode::LoanAlreadyDefaulted
+        );
+
+        let seeds = &[
+            b"loan".as_ref(),
+            &self.borrower_nft_mint.key().to_bytes()[..],
+            &self.platform.key().to_bytes()[..],
+            &[self.loan_account.bump],
+        ];
+        let signer_seeds = &[&seeds[..]];
+
+        let cpi_context = CpiContext::new_with_signer(
+            self.token_program.to_account_info(),
+            TransferChecked {
+                from: self.nft_vault.to_account_info(),
+                mint: self.borrower_nft_mint.to_account_info(),
+                to: self.lender_nft_ata.to_account_info(),
+                authority: self.loan_account.to_account_info(),
+            },
+            signer_seeds,
+        );
+
+        transfer_checked(cpi_context, 1, self.borrower_nft_mint.decimals)?;
+
+        self.loan_account.status = LoanStatus::Defaulted;
+
+        //closing nft_vault account
+        // close the nft_vault token account (returns rent to borrower)
+        let cpi_close = CpiContext::new_with_signer(
+            self.token_program.to_account_info(),
+            CloseAccount {
+                account: self.nft_vault.to_account_info(),
+                destination: self.borrower.to_account_info(),
+                authority: self.loan_account.to_account_info(),
+            },
+            signer_seeds,
+        );
+        close_account(cpi_close)?;
+
+        emit!(NFTClaimed {
+            loan: self.loan_account.key(),
+            borrower: self.borrower.key(),
+            nft_mint: self.borrower_nft_mint.key(),
+            timestamp: Clock::get()?.unix_timestamp,
+        });
+
+        Ok(())
+    }
+}
